c0e982bb0203e2d011a009c608e6a645a9b230b7
Ubuntu <ubuntu@ip-172-31-45-244.us-east-2.compute.internal>
2018-09-01 16:22:47 +0000

    Initialise

---
+++ app.js
@@ -0,0 +1,32 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log('Clock Bot Working');
+  client.user.setPresence({game: {name: "..an elf rogue.", type: 0}});
+});
+
+client.on('message', msg => {
+  var now = new Date()
+  var result = new Date(
+                 now.getFullYear(),
+                 now.getMonth(),
+                 now.getDate() + (7 + 0 - now.getDay()) % 7,
+                 19,
+                 0)
+
+  if (result < now){
+    result.setDate(result.getDate() + 7)
+  }
+
+  var difference = result.getTime() - now.getTime();
+  var check = now.getTime();
+  console.log(now.getDay()+" "+now.getDate() +" "+ now.toTimeString() + " - right meow");
+  console.log(result.getDay()+" "+result.getDate() + " "+ result.toTimeString()+" - later date");
+  var convert = new Date(difference);
+  console.log(convert.getDate());
+  console.log(convert.getDay()+" "+convert.toTimeString() + "conversion");
+});
+
+client.login('NDA5NjczMzMyOTUxMDg5MTcz.DViBog.QD1h2bpPHgTapGBwq9tNXlgjV-o');
